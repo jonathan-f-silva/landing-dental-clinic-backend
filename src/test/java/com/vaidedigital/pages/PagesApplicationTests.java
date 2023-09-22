9ecdb73ee676aedb4454ebab064163124f4021ad
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-22 18:20:06 -0300

    fix: tests context dirty each test

--- src/test/java/com/vaidedigital/pages/PagesApplicationTests.java
+++ src/test/java/com/vaidedigital/pages/PagesApplicationTests.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class PagesApplicationTests {
 
   @Autowired
