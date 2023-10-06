971b0304ff53980cf14614899af1c3fcd55b1f12
Jonathan Ferreira <j0n4t@hotmail.com>
2023-10-06 17:05:32 -0300

    feat: loginUser

--- src/main/java/com/vaidedigital/pages/controllers/UserController.java
+++ src/main/java/com/vaidedigital/pages/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.vaidedigital.pages.controllers;
 
 import com.vaidedigital.pages.dtos.CreateUserDto;
+import com.vaidedigital.pages.dtos.LoginDto;
 import com.vaidedigital.pages.entities.User;
+import com.vaidedigital.pages.services.AuthService;
 import com.vaidedigital.pages.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,21 @@ public class UserController {
 
   @Autowired
   UserService userService;
+
+  @Autowired
+  AuthService authService;
+
+  /**
+   * Generate a token for the given {@link Authentication}.
+   *
+   * @param loginDto the authentication data
+   * @return a token
+   */
+  @PostMapping("/login")
+  public String login(@RequestBody LoginDto loginDto) {
+    Authentication auth = authService.getAuth(loginDto);
+    return authService.getToken(auth);
+  }
 
   /**
    * Register a new user.
